446953f0d9f9d29a4e0d062e3bbc3ef89aba45e3
Artem Kalinchuk <artem9@gmail.com>
2024-04-05 09:41:32 -0400

    Adds octal to RELAY.asm

--- serial_board/RELAY.asm
+++ serial_board/RELAY.asm
@@ -2,28 +2,28 @@ RDA   EQU   01H
 TBE   EQU   02H
 
 ; INITIALISE
-        LDAI    03H       ;RESET 6850 UART
-        OUT1
-        LDAI    11H       ;8N2 
-        OUT1
+        LDAI    03H       ;006 003
+        OUT1              ;123
+        LDAI    11H       ;006 021 
+        OUT1              ;123
 
 ; INPUT ROUTINE
 INPUT:
-        IN0               ;READ STATUS REGISTER
-        LBA               ;LOAD STATUS TO B
-        LDAI    RDA
-        NDB               ;CHECK IF SERIAL PORT HAS DATA
-        JTZ     INPUT     ;IF NO DATA, KEEP ON CHECKING
-        IN1               ;IF DATA IS PRESENT, READ THE DATA
-        LCA               ;MOVE IT TO C
+        IN0               ;101
+        LBA               ;310
+        LDAI    RDA       ;006 001
+        NDB               ;241
+        JTZ     INPUT     ;150 006 000
+        IN1               ;103
+        LCA               ;320
 
 ; OUTPUT ROUTINE
 OUTPUT:
-        IN0               ;READ STATUS REGISTER
-        LBA               ;LOAD STATUS TO B
-        LDAI    TBE
-        NDB               ;CHECK IF SERIAL PORT CAN RECEIVE DATA
-        JTZ     OUTPUT    ;IF CANNOT, KEEP ON CHECKING
-        LAC               ;IF CAN, MOVE DATA FROM C BACK TO A
-        OUT2              ;OUTPUT A TO SERIAL PORT
-        JMP     INPUT     ;JUMP BACK TO LISTEN FOR MOVE INPUT
+        IN0               ;101
+        LBA               ;310
+        LDAI    TBE       ;006 002
+        NDB               ;241
+        JTZ     OUTPUT    ;150 020 000
+        LAC               ;302
+        OUT2              ;125
+        JMP     INPUT     ;104 006 000
